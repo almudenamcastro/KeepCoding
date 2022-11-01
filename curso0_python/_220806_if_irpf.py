538c48f5afe330f0cbc30bbf608e8853571a031c
Almudena M. Castro <almudena.m.castro@gmail.com>
2022-11-01 12:02:25 +0100

    sort and prettify curso0 IRPF and year-day exercises

--- curso0_python/_220806_if_irpf.py
+++ curso0_python/_220806_if_irpf.py
@@ -1,12 +1,31 @@
+# IF, ELIF
+# We can use if and elif to set a condition. 
+# The code will only be read if the condition is met. 
+
+# La diferencia entre if y elif es que: 
+# if siempre se evalúa (independientemente de la condición anterior)
+# elif solo se evalúa si la condición anterior era False
+
+def elifif(x):
+    if x > 1: #si es mayor que 1
+        print("es mayor que 1, if")
+    elif x > 0: #si NO es mayor que 1 Y ES mayor que 0 
+        print("es menor que 1 y mayor que 0, elif")
+    if x> 0: #si es mayor que 0 (luego también es mayor que 1)
+        print("es mayor que 0, if") 
+    else: 
+        print("else")
+
+# CALCULAR EL IRPF EN FUNCIÓN DE ESTADO CIVIL Y SUELDO
+
 # Queremos crear un programa que calcule la retención IRPF según estado civil, hijos y sueldo
 
-# pedir datos. 
-
+# Pedir datos: 
 estado_civil = input ("Situación (1, 2, 3): ")
 hijos = int(input("número de hijos: "))
 sueldo = float (input("Sueldo: "))
   
-# calcular exención.
+# Calcular exención:
 if estado_civil == '1':
     if hijos <1: 
         exencion = 0
@@ -29,67 +48,36 @@ elif estado_civil == '3':
     else: 
         exencion = 15093
 
-
-# calcular base a retener.
+# calcular base a retener:
 base = sueldo - exencion
 
-
-# calcular retención.
+# calcular retención:
 ret = 0
 
 #tramos irpf
 tramos = [0, 12450, 20200, 35200, 60000]
-porcent = [0.19, 0.24, 0.3, 0.37, 0.45]
+percent = [0.19, 0.24, 0.3, 0.37, 0.45]
 
-tramo4 = 60000
-p4 = 0.45
-tramo3 = 35200
-p3 = 0.37
-tramo2 = 20200
-p2 = 0.30
-tramo1 = 12450
-p1 = 0.24
-tramo0 = 0
-p0 = 0.19
+#vamos tramo a tramo reteniendo el porcentaje correspondiente. 
+if base - tramos[4] > 0: 
+    ret = (base - tramos[4]) * percent[4]
+    base = tramos[4]
 
+if base - tramos[3] > 0:
+    ret = ret + (base - tramos[3]) * percent[3]
+    base = tramos[3]
 
+if base - tramos[2] > 0:
+    ret=ret + (base - tramos[2]) * percent[2]
+    base = tramos[2]
 
-if base - tramo4 > 0: 
-    ret = (base - tramo4) * p4
-    base = tramo4
+if base - tramos[1] > 0:
+    ret=ret + (base - tramos[1]) * percent[1] 
+    base = tramos[1]
 
-if base - tramo3 > 0:
-    ret = ret + (base - tramo3) * p3
-    base = tramo3
-
-if base - tramo2 > 0:
-    ret=ret + (base - tramo2) * p2
-    base = tramo2
-
-if base - tramo1 > 0:
-    ret=ret + (base - tramo1) * p1 
-    base = tramo1
-
-if base - tramo0 >0:
-    ret = ret + (base - tramo0) * p0
-    base = tramo0
-
-
+if base - tramos[0] >0:
+    ret = ret + (base - tramos[0]) * percent[0]
+    base = tramos[0]
 
 #mostrar resultados
 print ("Retención: \t", ret)
-
-
-# nota: la diferencia entre if y elif es que: 
-# if siempre se evalúa (independientemente de la condición anterior)
-# elif solo se evalúa si la condición anterior era False
-
-def elifif(x):
-    if x > 1: #si es mayor que 1
-        print("es mayor que 1, if")
-    elif x > 0: #si NO es mayor que 1 Y ES mayor que 0 
-        print("es menor que 1 y mayor que 0, elif")
-    if x> 0: #si es mayor que 0 (luego también es mayor que 1)
-        print("es mayor que 0, if") 
-    else: 
-        print("else")
--- curso0_python/_220807_fucntions_year_day.py
+++ curso0_python/_220807_fucntions_year_day.py
@@ -1,13 +1,19 @@
+# WHAT YEAR DAY IS IT TODAY?
+# This program asks the user for a date and returns the number of days that have passed since the beginning of the year.
+
 month_length = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-# pedir fecha
+# ask for a date
 day = int(input("dia: "))
 month = int(input("mes: "))
 year = int(input("año: "))
 
-# ¿año bisiesto?
-
-def bisiesto (year):
+# is it a leap year?
+def is_leap (year):
+  '''
+  input: year (four digits)
+  output: boolean value, is it a leap-year? 
+  '''
   if year%4 != 0:
     return False
   if year%400 == 0: 
@@ -17,21 +23,21 @@ def bisiesto (year):
   else: 
     return True
   
-if bisiesto(year): 
-  month_length[1]=29
+if is_leap(year): 
+  month_length[1] = 29
 
-#contador meses anteriores
-i = 0
+# start counter
 day_count = 0
 
-while i < month - 1:
-#   day_count = day_count + month_length[i]
-    day_count += month_length[i]
+# add days of the passed months
+i = 1
+while i < month:
+    day_count += month_length[i-1]
     i += 1
 
-#calcular dia
+# add the passed days of the month
 day_count += day
 
-#imprimir resultado
+#pritn result
 print(day_count)
 
